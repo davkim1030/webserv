2d5342fdb7401eb673c9ecf2c8e5c4268286c04f
Joochan Kim <joockim@c6r10s4.42seoul.kr>
2021-05-27 16:47:19 +0900

    save

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -12,8 +12,41 @@
 
 #include "webserv.h"
 
+#define PORT 8888
+
 int		main(void)
 {
+	int serv_sock;
+	int clnt_sock;
+
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in clnt_addr;
+	socklen_t clnt_addr_size;
+
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (serv_sock == -1)
+		std::cout << "error : sock error" << std::endl;
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(PORT);
+
+	if (bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == 1)
+		std::cout << "error : bing error" << std::endl;
+	
+	if (listen(serv_sock, 5) == -1)
+		std::cout << "error : listen error" << std::endl;
+
+	clnt_addr_size = sizeof(clnt_addr);
+	clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+	if (clnt_sock == -1)
+		std::cout << "error : client accept error" << std::endl;
+	char message[] = "Hello test world\n";
+	write(clnt_sock, message, sizeof(message));
+
+	close(clnt_sock);
+	close(serv_sock);
+
 
 	return (0);
 }
